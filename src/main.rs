ae30cfb6fa6b32c319f6f79c5ad6d7f22ca78e69
qnvary27 <daniel.machtolf@proton.me>
2026-06-25 20:17:01 +0200

    Implemented Input and Input Error Handling https://github.com/qnavry27/rust-rock-paper-scissors/issues/4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+
 fn main() {
+    loop {
+        let mut player_input = String::new();
     println!("Welcome to Rock Paper Scissors!");
+    println!("Player, please decide.");
+
+    io::stdin()
+        .read_line(&mut player_input)
+        .expect("Failed to read input\n");
+
+    let player_input = player_input.trim().to_lowercase();
+
+    if player_input == "rock" || player_input == "paper" || player_input == "scissors" {
+        println!("You chose: {}", player_input);
+        break;
+    } else {
+       println!("Invalid input, try again.\n");
+        }
+    }
 }
+
