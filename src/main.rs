635ee6b3158c9e53db9ad293554267feb24d4396
qnvary27 <daniel.machtolf@proton.me>
2026-06-26 11:32:46 +0200

    Implemented Game Logic and Game Loop.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use std::io;
+use std::{io, process::exit};
 use rand::{self, seq::SliceRandom, thread_rng};
 
 fn main() {
     loop {
         let mut player_input = String::new();
-        let computer_options = ["Rock", "Paper", "Scissors"];
+        let computer_options = ["rock", "paper", "scissors"];
         let mut rng = thread_rng();
         println!("Welcome to Rock Paper Scissors!");
-        println!("Player, please decide.");
+        println!("Player, please decide:");
 
         io::stdin()
             .read_line(&mut player_input)
@@ -21,10 +21,33 @@ fn main() {
             println!("Invalid input, try again.\n");
             continue;
         }
-        if let Some(computer_pick) = computer_options.choose(&mut rng){
-            println!("The computer's choice is: {}\n ", computer_pick);
+         let computer_pick: &str = computer_options.choose(&mut rng).unwrap();
+         println!("Computer chose: {}", computer_pick);
+
+        if player_input == computer_pick {
+            println!("Its a draw!\n");
+        }
+        else if (player_input == "rock" && computer_pick == "scissors") ||
+        (player_input == "paper" && computer_pick == "rock") ||
+        (player_input == "scissors" && computer_pick == "paper") {
+            println!("You win!\n");
+        }
+        else {
+            println!("Computer wins!\n")
+        }
+
+        println!("Want to play again? (y(es)/n(o))");
+        let mut play_again = String::new();
+        io::stdin()
+            .read_line(  &mut play_again)
+            .expect("Failed to read input\n");
+        let play_again = play_again.trim().to_lowercase();
+        if play_again == "yes" || play_again == "y" {
+            println!("\n");
+            continue;
         } else {
-            println!("The list was empty");
+            println!("Goodbye!");
+            exit(0);
         }
     }
-}
+ }
